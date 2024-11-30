10b0231c2663086de514750412eda4cb02d7604c
DESKTOP-9KGUJ9U\naratteu <skfhddlg@naver.com>
2024-11-30 14:18:54 +0900

    Aspire Ollama - 시멘틱커널 임의연동

--- BlazorApp/Program.cs
+++ BlazorApp/Program.cs
@@ -1,7 +1,15 @@
 using BlazorApp.Components;
+using Microsoft.SemanticKernel;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.AddOllamaSharpChatClient("ollama-qwen");
+
+// https://github.com/CommunityToolkit/Aspire/blob/95b840a788e37ea95d2754bb9568b6418ddbd9ab/src/CommunityToolkit.Aspire.OllamaSharp/AspireOllamaSharpExtensions.cs#L111
+var ollama = new System.Data.Common.DbConnectionStringBuilder { ConnectionString = builder.Configuration.GetConnectionString("ollama-qwen") };
+#pragma warning disable SKEXP0070
+builder.Services.AddOllamaChatCompletion(
+    modelId: (string)ollama["Model"],
+    endpoint: new((string)ollama["Endpoint"])
+);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
