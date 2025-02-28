411c9638588999df642409138d798f2497d208d0
naratteu <skfhddlg@naver.com>
2025-02-28 17:09:12 +0900

    add notebook.ipynb

--- AspireHost/Program.cs
+++ AspireHost/Program.cs
@@ -1,7 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 // https://learn.microsoft.com/dotnet/aspire/community-toolkit/ollama
-var ollama = builder.AddOllama("ollama");
+var ollama = builder.AddOllama("ollama").WithEndpoint(11434, 11434);
 builder.AddProject<Projects.BlazorApp>(nameof(Projects.BlazorApp))
     .WithReference(ollama.AddModel("ollama-qwen", "qwen2.5:0.5b"));
 builder.Build().Run();
